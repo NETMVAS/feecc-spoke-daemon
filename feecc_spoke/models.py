44b130162e6ee1fd58e8207de9e965b8b920c043
arseniiarsenii <arseniivelichko2@gmail.com>
2021-09-27 22:57:58 +0300

    added models, startup and shutdown event handling

--- feecc_spoke/models.py
+++ feecc_spoke/models.py
@@ -1,6 +1,16 @@
 from pydantic import BaseModel
 
 
-class BaseEvent(BaseModel):
+class GenericResponse(BaseModel):
+    status: bool
+    comment: str
+
+
+class HidEvent(BaseModel):
     name: str
     string: str
+
+
+class HidBufferEntry(GenericResponse):
+    buffer: str
+    added_on: float
--- main.py
+++ main.py
@@ -1,29 +1,35 @@
-import atexit
 import typing as tp
 from time import time
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
-import uvicorn
 
 from _logging import CONSOLE_LOGGING_CONFIG, FILE_LOGGING_CONFIG
 from feecc_spoke.Display import Display
 from feecc_spoke.Employee import Employee
 from feecc_spoke.Exceptions import StateForbiddenError
 from feecc_spoke.Spoke import Spoke
-from feecc_spoke.Types import RequestPayload
-from feecc_spoke.models import BaseEvent
+from feecc_spoke.models import HidEvent, HidBufferEntry, GenericResponse
 
 logger.configure(handlers=[CONSOLE_LOGGING_CONFIG, FILE_LOGGING_CONFIG])
-
 api = FastAPI()
-
 api.add_middleware(CORSMiddleware, allow_origins=["*"])
 
 
-@atexit.register
-def end_session() -> None:
+@api.on_event("startup")
+@logger.catch
+def startup_event() -> None:
+    """tasks to do at server startup"""
+    Display()
+    logger.info("Syncing login status")
+    Spoke().sync_login_status()
+
+
+@api.on_event("shutdown")
+@logger.catch
+def shutdown_event() -> None:
     """log out the worker, clear the display, release SPI and join the thread before exiting"""
     logger.info("SIGTERM handling started")
     if Employee().is_authorized:
@@ -33,8 +39,8 @@ def end_session() -> None:
     logger.info("SIGTERM handling finished")
 
 
-@api.post("/api/hid_event")
-def handle_hid_event(event: BaseEvent) -> RequestPayload:
+@api.post("/api/hid_event", response_model=GenericResponse)
+def handle_hid_event(event: HidEvent) -> GenericResponse:
     """Parse the event dict JSON"""
     logger.debug(f"Received event dict:\n{event.json()}")
     # handle the event in accord with it's source
@@ -53,29 +59,26 @@ def handle_hid_event(event: BaseEvent) -> RequestPayload:
         else:
             message: str = "Sender of the event dict is not mentioned in the config. Can't handle the request."
             logger.error(message)
-            return {"status": False, "comment": message}
+            return GenericResponse(status=False, comment=message)
 
-        return {"status": True, "comment": "Hid event has been handled as expected"}
+        return GenericResponse(status=True, comment="Hid event has been handled as expected")
 
     except StateForbiddenError as E:
-        return {"status": False, "comment": f"operation is forbidden by the state: {E}"}
+        return GenericResponse(status=False, comment=f"operation is forbidden by the state: {E}")
 
 
-@api.get("/api/hid_buffer")
-def get_latest_buffer_entry() -> RequestPayload:
+@api.get("/api/hid_buffer", response_model=HidBufferEntry)
+def get_latest_buffer_entry() -> HidBufferEntry:
     """get latest buffer entry"""
-    return {
-        "status": True,
-        "comment": "Retrieved HID buffer",
-        "buffer": Spoke().hid_buffer,
-        "added_on": Spoke().hid_buffer_added_on,
-    }
+    return HidBufferEntry(
+        status=True,
+        comment="Retrieved HID buffer",
+        buffer=Spoke().hid_buffer,
+        added_on=Spoke().hid_buffer_added_on,
+    )
 
 
 if __name__ == "__main__":
-    Display()
-    logger.info("Syncing login status")
-    Spoke().sync_login_status()
     logger.info("Starting server")
     server_ip: str = Spoke().config["api"]["server_ip"]
     server_port: int = int(Spoke().config["api"]["server_port"])
